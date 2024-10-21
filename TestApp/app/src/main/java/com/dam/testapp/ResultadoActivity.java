7931f712cc2d2c6cef0cb08ab118f37b29fd977c
Carlos <dominguezalacid@gmail.com>
2024-10-21 10:41:42 +0200

    actualizar

--- TestApp/app/src/main/java/com/dam/testapp/ResultadoActivity.java
+++ TestApp/app/src/main/java/com/dam/testapp/ResultadoActivity.java
@@ -1,6 +1,9 @@
 package com.dam.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,18 +15,51 @@ public class ResultadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resultado);
 
         TextView textoResultado = findViewById(R.id.texto_resultado);
+        Button botonReintentar = findViewById(R.id.button);
 
         // Obtener las respuestas
         String respuesta1 = getIntent().getStringExtra("respuesta1");
         String respuesta2 = getIntent().getStringExtra("respuesta2");
         String respuesta3 = getIntent().getStringExtra("respuesta3");
 
+        String respCorrecta1 = "setContentView()";
+        String respCorrecta2 = "onStart()";
+        String respCorrecta3 = "onCreate()";
+
+        String resultado1;
+        String resultado2;
+        String resultado3;
+
+        if(respuesta1.equals(respCorrecta1)){
+            resultado1 = "Correcto";
+        }else{
+            resultado1 = "Incorrecto";
+        }
+
+        if(respuesta2.equals(respCorrecta2)){
+            resultado2 = "Correcto";
+        }else{
+            resultado2 = "Incorrecto";
+        }
+
+        if(respuesta3.equals(respCorrecta3)){
+            resultado3 = "Correcto";
+        }else{
+            resultado3 = "Incorrecto";
+        }
+
         // Mostrar resultado
         String resultado = "Respuestas del test:\n\n" +
-                "Pregunta 1: " + respuesta1 + "\n" +
-                "Pregunta 2: " + respuesta2 + "\n" +
-                "Pregunta 3: " + respuesta3;
+                "Pregunta 1: " + respuesta1 + resultado1 + "\n" +
+                "Pregunta 2: " + respuesta2 + resultado2 + "\n" +
+                "Pregunta 3: " + respuesta3 + resultado3;
 
         textoResultado.setText(resultado);
+    }
+
+    public void reintentar(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
